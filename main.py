ff143fdfdfb3249da4c4db52cccedacf1be93ecb
Dirbeq <dirbeq@gmail.com>
2022-03-17 11:01:58 +0100

    Update main.py
    time.sleep

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import mouse
+import mouse,time
 
 while True:
     try:
         pos = mouse.get_position
         print(pos(),end='\r')
+        time.sleep(0.05) # Prevents the processor from exploding
     except:
         break
